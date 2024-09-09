117ef3c70e8126678b18530acb1716652164459e
MDar123 <mujtabadar98@gmail.com>
2024-09-09 22:25:11 +0500

    half completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './about/about.component';
-import { HeroComponent } from './hero/hero.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
 
 /* Added routing here : */
 const routes: Routes = [
-{path:'',redirectTo:'home',pathMatch:'full'},
-{path:'home',component:HeroComponent},
-{path:'about',component:AboutComponent},
-{path:'portfolio',component:PortfolioComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,33 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
-import { HeroComponent } from './hero/hero.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { ListComponent } from './list/list.component';
 import { RouterLink, RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    AboutComponent,
-    PortfolioComponent,
-    FooterComponent,
-    HeaderComponent,
-    HeroComponent,
-    ListComponent,
   ],
   imports:[
     BrowserModule,
     AppRoutingModule,
     NgbModule,
     RouterLink,
-    RouterModule,
+    RouterModule
   ],
   providers: [
     provideClientHydration(),
